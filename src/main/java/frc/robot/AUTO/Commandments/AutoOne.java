767bc89c263cb7e2711a42913b22a0458ca3cd73
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2023-03-07 08:26:33 -0800

    claw auto

--- src/main/java/frc/robot/AUTO/Commandments/AutoOne.java
+++ src/main/java/frc/robot/AUTO/Commandments/AutoOne.java
@@ -7,6 +7,7 @@ package frc.robot.AUTO.Commandments;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.AUTO.Commands.ArmCommand;
+import frc.robot.AUTO.Commands.ClawCommand;
 import frc.robot.AUTO.Commands.MotionMagicAuto;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -18,6 +19,6 @@ public class AutoOne extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      Commands.deadline(new MotionMagicAuto(20, 0), new ArmCommand(90).withTimeout(6)));
+      Commands.deadline(new MotionMagicAuto(20, 0), new ArmCommand(90).withTimeout(6), new ClawCommand(true).withTimeout(6)));
   }
 }
